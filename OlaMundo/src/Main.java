e2058d426a68e13204cad6e0aa4eb7f72444797f
Enrico Bernz Reichow Santos <71547@PROWAY.TREINA>
2022-01-31 21:52:59 -0300

    first commit

---
+++ OlaMundo/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Olá Mundo");
+		
+		String nome = "Enrico";
+		System.out.println(nome);
+		
+		System.out.println();
+		
+		String nomeAluno = "Enrico";
+				
+		Integer idadeAluno = 17;
+		
+		Double alturaAluno = 1.75;
+		
+		Boolean presenca = true;
+		
+		System.out.println("O nome do aluno é " + nomeAluno);
+		System.out.println("A idade do aluno é " + idadeAluno);
+		System.out.println("A altura do aluno é " + alturaAluno);
+		System.out.println("Aluno " +nomeAluno+ " está presente? - " +presenca);
+		
+		
+	}
+
+}
